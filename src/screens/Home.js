f587c579693d61308259ce97136368c1f8c55bff
Mason3144 <lts890303@gmail.com>
2022-10-18 19:43:43 +0900

    Instargram portfolio

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -5,6 +5,10 @@ import { Link } from "react-router-dom";
 
 const BigContainer = styled.div``;
 const Container = styled.div``;
+const H2 = styled.h2`
+  color: rgba(0, 0, 0, 0.7);
+  text-decoration: underline;
+`;
 
 export default function Home() {
   return (
@@ -12,8 +16,8 @@ export default function Home() {
       <Header></Header>
       <Layout>
         <Container>
-          <Link to={"/instagram-clone"}>
-            <h2>Instagram clone coding (IOS)</h2>
+          <Link to={"/instagram-clone"} style={{ textDecoration: "none" }}>
+            <H2>Instagram clone coding (IOS)</H2>
           </Link>
         </Container>
       </Layout>
